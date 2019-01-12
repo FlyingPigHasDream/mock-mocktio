f3f9243230cc68a58cbda25dbdd9523909da0e86
Rain <l>
2019-01-12 10:11:37 +0800

    Initial commit

---
+++ src/main/java/org/mock/mockito/MockMocktioApplication.java
@@ -0,0 +1,14 @@
+package org.mock.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMocktioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockMocktioApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/mock/mockito/MockMocktioApplicationTests.java
@@ -0,0 +1,17 @@
+package org.mock.mockito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockMocktioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
